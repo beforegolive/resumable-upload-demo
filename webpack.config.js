9971bdc2e57da0fdba3384235df5fb998a2bdff3
twomeetings <2326220@163.com>
2020-04-09 17:06:58 +0800

    add react app

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin= require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+module.exports={
+  mode:'development',
+  entry:'./src/index.js',
+  output:{
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle-[chunkhash:8].js'
+  },
+  module:{
+    rules:[
+      { 
+        test: /.jsx?$/, 
+        use:'babel-loader',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: path.resolve(__dirname,'src/index.html')}),
+    new CleanWebpackPlugin()
+  ]
+}
